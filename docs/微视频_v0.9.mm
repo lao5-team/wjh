1ac1218533bfa6bce6226d473ffb4f63ac0d2f2e
yihao <yhchinabest@gmail.com>
2015-02-28 09:47:52 +0800

    upload docs

---
+++ docs/微视频_v0.9.mm
@@ -0,0 +1,158 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1425083169770" ID="ujh375Kx" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+        <head/>
+        <body>
+          <p>&#x5fae;&#x89c6;&#x9891;</p>
+        </body>
+      </html></richcontent>
+<node CREATED="1425083169770" ID="fl8Axd0C" MODIFIED="1425083169770" POSITION="right">
+<richcontent TYPE="NODE"><html>
+          <head/>
+          <body>
+            <p>&#x89c6;&#x9891;</p>
+          </body>
+        </html></richcontent>
+<node CREATED="1425083169770" ID="b5EYbQ7P" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+            <head/>
+            <body>
+              <p>&#x89e3;&#x51b3;&#x65b9;&#x6cd5;&#x53c2;&#x8003;</p>
+            </body>
+          </html></richcontent>
+<node CREATED="1425083169770" ID="mbQf6RG4" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+              <head/>
+              <body>
+                <p>http://www.open-open.com/lib/view/open1389170927953.html</p>
+              </body>
+            </html></richcontent>
+</node>
+<node CREATED="1425083169770" ID="JgpxrpBz" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+              <head/>
+              <body>
+                <p>http://www.eoeandroid.com/thread-177893-1-1.html</p>
+              </body>
+            </html></richcontent>
+</node>
+<node CREATED="1425083169770" ID="Rf7IHUIW" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+              <head/>
+              <body>
+                <p>GPUImage</p>
+              </body>
+            </html></richcontent>
+</node>
+<node CREATED="1425083169770" ID="ZemSKWbw" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+              <head/>
+              <body>
+                <p>http://bigflake.com/mediacodec/</p>
+              </body>
+            </html></richcontent>
+</node>
+<node CREATED="1425085082517" ID="ID_416258714" MODIFIED="1425085086304" TEXT="https://github.com/CyberAgent/android-gpuimage"/>
+<node CREATED="1425086038692" ID="ID_455705834" MODIFIED="1425086040347" TEXT="http://www.cnblogs.com/over140/p/3704580.html"/>
+</node>
+<node CREATED="1425083169770" ID="mb4wwqkA" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+            <head/>
+            <body>
+              <p>&#x4ea7;&#x54c1;&#x5bf9;&#x6807;</p>
+            </body>
+          </html></richcontent>
+<node CREATED="1425083169770" ID="zv8RYVlX" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+              <head/>
+              <body>
+                <p>Instagram</p>
+              </body>
+            </html></richcontent>
+</node>
+<node CREATED="1425083169770" ID="fEaeeSAz" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+              <head/>
+              <body>
+                <p>&#x7f8e;&#x62cd;</p>
+              </body>
+            </html></richcontent>
+</node>
+<node CREATED="1425083169770" ID="AZxgKKgj" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+              <head/>
+              <body>
+                <p>&#x5feb;&#x624b;</p>
+              </body>
+            </html></richcontent>
+</node>
+<node CREATED="1425083169770" ID="BEIKjiYF" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+              <head/>
+              <body>
+                <p>NewSubNode</p>
+              </body>
+            </html></richcontent>
+</node>
+</node>
+<node CREATED="1425083169770" ID="cIArPy2w" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+            <head/>
+            <body>
+              <p>&#x529f;&#x80fd;</p>
+            </body>
+          </html></richcontent>
+<node CREATED="1425083169770" ID="NHR1Wizw" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+              <head/>
+              <body>
+                <p>&#x62cd;&#x7167;</p>
+              </body>
+            </html></richcontent>
+<node CREATED="1425083169770" ID="NPteRsH2" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+                <head/>
+                <body>
+                  <p>&#x6ee4;&#x955c;</p>
+                </body>
+              </html></richcontent>
+<node CREATED="1425083809868" ID="ID_1028241330" MODIFIED="1425083833394" TEXT="&#x589e;&#x5f3a;&#x5bf9;&#x6bd4;&#x5ea6;contrast"/>
+</node>
+</node>
+<node CREATED="1425083169770" ID="bjf8nSh2" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+              <head/>
+              <body>
+                <p>&#x89c6;&#x9891;</p>
+              </body>
+            </html></richcontent>
+<node CREATED="1425083169770" ID="rvMBqu0X" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+                <head/>
+                <body>
+                  <p>&#x591a;&#x5f20;&#x56fe;&#x7247;&#x5408;&#x6210;</p>
+                </body>
+              </html></richcontent>
+</node>
+<node CREATED="1425083169770" ID="U6O2KiCV" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+                <head/>
+                <body>
+                  <p>&#x89c6;&#x9891;&#x52a0;&#x5165;&#x97f3;&#x8f68;</p>
+                </body>
+              </html></richcontent>
+</node>
+<node CREATED="1425083169770" ID="GnV44OoF" MODIFIED="1425083169770">
+<richcontent TYPE="NODE"><html>
+                <head/>
+                <body>
+                  <p>&#x6ee4;&#x955c;</p>
+                </body>
+              </html></richcontent>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
